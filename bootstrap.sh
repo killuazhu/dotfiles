3573c5a773a7a55325aef904b7f691249b2cf3a4
Kyle Zhu <xianjun@us.ibm.com>
2020-06-03 03:37:45 +0000

    Add nvim config

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Mainly referenced from https://github.com/mathiasbynens/dotfiles/blob/master/bootstrap.sh
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
+
+	# setup symlink for nvim
+	mkdir -p ~/.config/nvim
+	ln -s ~/.vimrc ~/.config/nvim/init.vim
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
