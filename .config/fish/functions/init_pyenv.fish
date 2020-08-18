adf1fc2d43bca528fec6c5515eb61c4c32ed754a
Xianjun Zhu <xianjun@us.ibm.com>
2020-08-18 09:14:37 -0400

    Do not activate virtual env

--- .config/fish/functions/init_pyenv.fish
+++ .config/fish/functions/init_pyenv.fish
@@ -2,6 +2,6 @@ function init_pyenv --description "Init pyenv and enable shell integration"
   # Enable pyenv shim and autocompletion
   if which pyenv
     status --is-interactive; and pyenv init - | source
-    status --is-interactive; and pyenv virtualenv-init - | source
+    # status --is-interactive; and pyenv virtualenv-init - | source
   end
 end
