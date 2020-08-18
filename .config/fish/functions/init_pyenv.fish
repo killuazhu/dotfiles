5317512c95361e6ebc5abd4946fd9693b92444d6
Xianjun Zhu <xianjun@us.ibm.com>
2020-08-18 09:15:47 -0400

    Quiet pyenv check

--- .config/fish/functions/init_pyenv.fish
+++ .config/fish/functions/init_pyenv.fish
@@ -1,6 +1,6 @@
 function init_pyenv --description "Init pyenv and enable shell integration"
   # Enable pyenv shim and autocompletion
-  if which pyenv
+  if which pyenv > /dev/null
     status --is-interactive; and pyenv init - | source
     # status --is-interactive; and pyenv virtualenv-init - | source
   end
